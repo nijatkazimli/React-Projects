0dc1ddf8f7952fe92f3c378b1f0c60dccd2924ff
nijatkazimli <nctkzmli@gmail.com>
2023-10-03 09:46:54 +0200

    first three tasks

--- react_lab1/src/App.js
+++ react_lab1/src/App.js
@@ -1,9 +1,38 @@
 import './App.css';
 
+function filterNamesWithStartingM(names) {
+  return names.filter(name => name.startsWith('M'))
+}
+
+function squareRootArray(arr) {
+  return arr.map(number => Math.sqrt(number));
+}
+
+function sumArray(arr) {
+  return arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
+
 function App() {
-  return (
-    <div className="App">
-      <p>My first react component</p>
+  const students = ['John', 'Mark', 'Stephanie', 'Martha', 'Vlad'];
+  const numbers = [2, 5, 8, 10];
+  const sum = sumArray(numbers)
+  const filteredStudents = filterNamesWithStartingM(students)
+  const rootedNumbers = squareRootArray(numbers)
+  return (    
+    <div>
+      <ul>
+        {filteredStudents.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+      <ul>
+        {rootedNumbers.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+      <p>
+        {sum}
+      </p>
     </div>
   );
 }
