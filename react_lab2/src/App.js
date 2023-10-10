c2ee7d4108dbbcebba10a4a06a2da67ee0a99e63
nijatkazimli <nctkzmli@gmail.com>
2023-10-10 08:53:41 +0200

    styling

--- react_lab2/src/App.js
+++ react_lab2/src/App.js
@@ -9,6 +9,7 @@ function App() {
   return (
     <div className="app-container">
       <header className="app-header">
+        <h1>Character Directory</h1>
         <SearchBar />
       </header>
       <main className="app-main">
