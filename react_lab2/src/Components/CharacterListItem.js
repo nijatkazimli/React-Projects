0c8ac9a2d07e5b98ad9f280ddfcf62e2b48c7fca
nijatkazimli <nctkzmli@gmail.com>
2023-10-15 08:01:59 +0200

    Ex3

--- react_lab2/src/Components/CharacterListItem.js
+++ react_lab2/src/Components/CharacterListItem.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import './CharacterListItem.css';
 
 function CharacterListItem({ number, person }) {
   return (
     <li>
-      <p>{number + 1}. {person.name} {person.email}</p>
-      <img src={person.avatarUrl}></img>
+      <div className="character-container">
+        <img className="avatar" src={person.avatarUrl} alt={person.name} />
+        <div className="info">
+          <p>{number + 1}. {person.name}</p>
+          <p>{person.email}</p>
+        </div>
+        <div className="buttons">
+          <button className="delete-button">Delete</button>
+          <button className="message-button">Message</button>
+        </div>
+      </div>
     </li>
   );
 }
