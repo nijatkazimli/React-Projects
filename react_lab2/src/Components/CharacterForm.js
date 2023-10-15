6280d9773dd176c6819b8727d0fa9b68159ef02f
nijatkazimli <nctkzmli@gmail.com>
2023-10-15 08:13:39 +0200

    Lab2Ex4

--- react_lab2/src/Components/CharacterForm.js
+++ react_lab2/src/Components/CharacterForm.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './CharacterForm.css';
 
 function CharacterForm() {
   return (
-    <div>
-      <h2>CharacterForm Component</h2>
-      <p>I'm CharacterForm Component</p>
+    <div className="character-form">
+      <h2>Add Character</h2>
+      <form className="add-form">
+        <div className="input-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" placeholder="Enter name" />
+        </div>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" id="email" placeholder="Enter email" />
+        </div>
+        <div className="input-group">
+          <label htmlFor="avatar">Avatar URL</label>
+          <input type="url" id="avatar" placeholder="Enter avatar URL" />
+        </div>
+        <button className="add-button">Add</button>
+      </form>
     </div>
   );
 }
