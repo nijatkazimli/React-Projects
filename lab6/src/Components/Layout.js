030bdb8f2542721b264cf985c771d0e92a070d53
nijatkazimli <nctkzmli@gmail.com>
2023-11-13 23:16:12 +0100

    Lab6Ex6

--- lab6/src/Components/Layout.js
+++ lab6/src/Components/Layout.js
@@ -1,8 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { NavLink, Outlet } from 'react-router-dom';
+import "./Layout.css"
 
 const Layout = () => {
     return (
         <div>
+            <div className="nav-container">
+                <NavLink
+                    to="/"
+                    className={({ isActive }) => 
+                    isActive ? "active" : ""
+                    }
+                >
+                    Home Page
+                </NavLink>
+                <NavLink
+                    to="/about"
+                >
+                    About
+                </NavLink>
+            </div>
             <Outlet />
         </div>
     )
