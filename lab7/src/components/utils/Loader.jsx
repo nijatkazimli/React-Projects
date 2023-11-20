e30746bf976a95bbc0136d11fe8dbe289210b339
nijatkazimli <nctkzmli@gmail.com>
2023-11-21 00:42:40 +0100

    Lab7Ex2

--- lab7/src/components/utils/Loader.jsx
+++ lab7/src/components/utils/Loader.jsx
@@ -1,10 +1,12 @@
+// src/components/utils/Loader.tsx
 import React from 'react';
 
-const Loader = ({loading, label, children}) => {
-
-    return (
-        <></>
-    )
-}
+const Loader = ({ loading, label, children }) => {
+  return loading ? (
+    <>{label ? `${label}...` : 'Loading...'}</>
+  ) : (
+    <>{children}</>
+  );
+};
 
 export default Loader;
