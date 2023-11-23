4e51aa7e071f044d1a21193eed79fb54c161aefc
nijatkazimli <nctkzmli@gmail.com>
2023-11-23 22:26:53 +0100

    Lab7Ex2andEx3

--- lab7/src/components/App.tsx
+++ lab7/src/components/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import EmployeeList from "./employeeList/EmployeeList";
 
 const App = () => (
-    <></>
+    <>
+        <EmployeeList/>
+    </>
 )
 
 export default App;
