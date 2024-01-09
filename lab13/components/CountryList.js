7596663de1ced9c5d9cfc43c417c28bdfb7d3ae5
nijatkazimli <nctkzmli@gmail.com>
2024-01-09 09:16:00 +0100

    Lab13Ex3

--- lab13/components/CountryList.js
+++ lab13/components/CountryList.js
@@ -1,20 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, FlatList, TouchableOpacity, ActivityIndicator } from 'react-native';
+import {
+  View,
+  Text,
+  FlatList,
+  TouchableOpacity,
+  TextInput,
+  ActivityIndicator,
+} from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 
 const CountryList = () => {
   const [countries, setCountries] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    fetch('https://restcountries.com/v3.1/all')
-      .then(response => response.json())
-      .then(data => {
+  const fetchAllCountries = async () => {
+    try {
+      setLoading(true);
+
+      const response = await fetch('https://restcountries.com/v3.1/all');
+      const data = await response.json();
+      setCountries(data);
+    } catch (error) {
+      console.error('Error fetching all countries:', error);
+      setCountries([]);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const searchCountries = async (query) => {
+    try {
+      if (query.length >= 3) {
+        setLoading(true);
+        const response = await fetch(`https://restcountries.com/v3.1/name/${query}`);
+        const data = await response.json();
         setCountries(data);
-        setLoading(false);
-      })
-      .catch(error => console.error('Error fetching countries:', error));
-  }, []);
+      } else if (query.length === 0) {
+        fetchAllCountries();
+      }
+    } catch (error) {
+      console.error('Error searching countries:', error);
+      setCountries([]);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const navigation = useNavigation();
 
@@ -26,16 +56,21 @@ const CountryList = () => {
     </TouchableOpacity>
   );
 
+  useEffect(() => {
+    fetchAllCountries();
+  }, []);
+
   return (
     <View>
+      <TextInput
+        style={{ height: 40, borderColor: 'gray', borderWidth: 1, margin: 10, padding: 10 }}
+        placeholder="Search countries..."
+        onChangeText={(text) => searchCountries(text)}
+      />
       {loading ? (
         <ActivityIndicator size="large" color="#0000ff" />
       ) : (
-        <FlatList
-          data={countries}
-          keyExtractor={(item) => item.cca2}
-          renderItem={renderCountry}
-        />
+        <FlatList data={countries} keyExtractor={(item) => item.cca2} renderItem={renderCountry} />
       )}
     </View>
   );
